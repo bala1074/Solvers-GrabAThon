cbd85a3df583c2386ef268b12261dc517fdd75bf
Bala <balasatish@colortokens.com>
2019-12-07 22:30:44 +0530

    V1 of pay is done

--- app/src/main/java/com/example/myapplication/ui/Pay/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/Pay/HomeFragment.kt
@@ -9,6 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.myapplication.R
+import android.os.CountDownTimer
+
+
+
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +30,47 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
+        val textViewTimer: TextView = root.findViewById(R.id.text_home2)
+        homeViewModel.text.observe(this, Observer {
+            textViewTimer.text = it
+        })
+
+        val twoFactorAuthUtil = TwoFactorAuthUtil()
+
+        // String base32Secret = twoFactorAuthUtil.generateBase32Secret();
+        val base32Secret = "NY4A5CPJZ46LXZCP"
+
+        println("secret = $base32Secret")
+
+        // this is the name of the key which can be displayed by the authenticator
+        // program
+        val keyId = "user@j256.com"
+        // generate the QR code
+        //System.out.println("Image url = " + twoFactorAuthUtil.qrImageUrl(keyId, base32Secret))
+        // we can display this image to the user to let them load it into their auth
+        // program
+
+        // we can use the code here and compare it against user input
+        var code = twoFactorAuthUtil.generateCurrentNumber(base32Secret)
+
+        /*
+		 * this loop shows how the number changes over time
+		 */
+
+        object : CountDownTimer(300000000000000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val diff =
+                    TwoFactorAuthUtil.TIME_STEP_SECONDS - System.currentTimeMillis() / 1000 % TwoFactorAuthUtil.TIME_STEP_SECONDS
+                code = twoFactorAuthUtil.generateCurrentNumber(base32Secret)
+                textView.setText("OTP: "+code)
+                textViewTimer.setText("OTP expires in "+diff);
+            }
+
+            override fun onFinish() {
+                textView.setText("done!")
+            }
+        }.start()
+
         return root
     }
 }
